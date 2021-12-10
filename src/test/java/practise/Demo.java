aaf9133967b9e687dcabebce27d1b511145b9242
Hp <Hp@DESKTOP-8SJFKNP>
2021-12-10 09:42:13 +0530

    major changes

--- src/test/java/practise/Demo.java
+++ src/test/java/practise/Demo.java
@@ -7,7 +7,9 @@ public class Demo {
 public void create() {
 	System.out.println("This is user-1,Script-1");
 	System.out.println("This is new user-2,updated Script-2");
+
 	System.out.println("This is version-1.0,"); 
+	System.out.println("3.1.5.9");
 	System.out.println("thisbjwguwu9df");
 	
 	
